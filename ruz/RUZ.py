d03ee6ac603d91cb192c6c8682cd34b38d16c5fa
Alex <eshobee@gmail.com>
2017-10-14 11:42:25 +0300

    add aliases for all API methods to RUZ, update docs

--- ruz/RUZ.py
+++ ruz/RUZ.py
@@ -6,7 +6,11 @@ from .utils import RUZ_API_ENDPOINTS, RUZ_API_URL, Logger
 
 
 class RUZ(object):
-    ''' Handler for RUZ API '''
+    '''
+        Handler for RUZ API
+
+        All methods are transformed from CamelCase to _ notation.
+    '''
 
     def __init__(self, strict_v1: bool=False, **kwargs):
         '''
@@ -14,6 +18,10 @@ class RUZ(object):
             Traceback (most recent call last):
                 ...
             PermissionError: Can't get base url!
+            >>> RUZ()._url2[-2]
+            '2'
+            >>> RUZ(strict_v1=True)._url2 == RUZ()._url
+            True
         '''
         self._url = kwargs.pop('base_url', RUZ_API_URL)
         if not self._url:
@@ -27,7 +35,14 @@ class RUZ(object):
 
     @property
     def v(self) -> int:
-        ''' Max API version '''
+        '''
+            Max API version
+
+            >>> RUZ().v
+            2
+            >>> RUZ(strict_v1=True).v
+            1
+        '''
         return 2 if self._url2[-2] == "2" else 1
 
     def _make_url(self, endpoint: str, data: dict=None, v: int=1) -> str:
@@ -54,6 +69,120 @@ class RUZ(object):
         except error.HTTPError as excinfo:
             self._logger.error(excinfo)
             return {}
+
+    def schedule(self, from_date: str, to_date: str, email: str=None,
+                 **params) -> dict:
+        '''
+            Return classes schedule.
+
+            :param from_date: str, required. Start of the period YYYY.MM.DD.
+            :param to_date: str, required. End of the period YYYY.MM.DD.
+            :param receiverType: int. Type of the schedule
+                (1/2/3 for teacher/auditorium/student).
+            :param groupOid: int. ID of group.
+            :param lecturerOid: int. ID of teacher.
+            :param auditoriumOid: int. ID of auditorium.
+            :param studentOid: int. ID of student.
+            :param email: str. Email on hse.ru (edu.hse.ru for students).
+
+            One of the followed required: lecturerOid, groupOid, auditoriumOid,
+                studentOid, email.
+        '''
+        return self.get("schedule", fromDate=from_date, toDate=to_date,
+                        email=email, data=params)
+
+    def groups(self, **params) -> dict:
+        '''
+            Return list of groups.
+
+            :param facultyOid: int. Course ID.
+            :param findText: str. Text to find.
+        '''
+        return self.get("groups", data=params)
+
+    def staff_of_group(self, group_id: int, **params) -> dict:
+        '''
+            Return list of students in group.
+
+            :param group_id: int, required. Group' ID.
+            :param findText: str. Text to find.
+        '''
+        return self.get("staffOfGroup", groupOid=group_id, data=params)
+
+    def streams(self, **params) -> dict:
+        '''
+            Return list of study streams.
+
+            :param findText: str. Text to find.
+        '''
+        return self.get("streams", data=params)
+
+    def staffOfStreams(self, stream_id: int, **params) -> dict:
+        '''
+            Return list of the groups on study stream.
+
+            :param stream_id: int, required. Group' ID.
+        '''
+        return self.get("staffOfStreams", streamOid=stream_id, data=params)
+
+    def lecturers(self, **params) -> dict:
+        '''
+            Return list of teachers.
+
+            :param chairOid: int. ID of department.
+            :param findText: str. Text to find.
+        '''
+        return self.get("lecturers", data=params)
+
+    def auditoriums(self, **params) -> dict:
+        '''
+            Return list of auditoriums.
+
+            :param buildingOid: int. ID of building.
+            :param findText: str. Text to find.
+        '''
+        return self.get("auditoriums", data=params)
+
+    def typeOfAuditoriums(self) -> dict:
+        ''' Return list of auditoriums' types. '''
+        return self.get("typeOfAuditoriums")
+
+    def kindOfWorks(self) -> dict:
+        ''' Return list of classes' types. '''
+        return self.get("kindOfWorks")
+
+    def buildings(self, **params) -> dict:
+        '''
+            Return list of buildings.
+
+            :param findText: str. Text to find.
+        '''
+        return self.get("buildings", data=params)
+
+    def faculties(self, **params) -> dict:
+        '''
+            Return list of learning programs (faculties).
+
+            :param findText: str. Text to find.
+        '''
+        return self.get("faculties", data=params)
+
+    def chairs(self, **params) -> dict:
+        '''
+            Return list of departments.
+
+            :param facultyOid: int. ID of course (learning program).
+            :param findText: str. Text to find.
+        '''
+        return self.get("chairs", data=params)
+
+    def subGroups(self, **params) -> dict:
+        '''
+            Return list of subgroups.
+
+            :param findText: str. Text to find.
+        '''
+        return self.get("subGroups", data=params)
 
 
 if __name__ == "__main__":
