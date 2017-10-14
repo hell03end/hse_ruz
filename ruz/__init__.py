b2dae9ea8a2036809f1f970e5eaf8f35e626f0de
Alex <eshobee@gmail.com>
2017-10-14 12:32:34 +0300

    (bug) fix docstring in ruz module

--- ruz/__init__.py
+++ ruz/__init__.py
@@ -1,11 +1,20 @@
+'''
+    Python wrapper for HSE RUZ API.
+
+    Usage:
+        from ruz import RUZ
+
+        api = RUZ()
+        assert api.v == 2
+        assert api.get("buildings")
+'''
+
 import os
 
 from .utils import RUZ_API_ENDPOINTS, RUZ_API_URL
 from .RUZ import RUZ
 
 
-__doc__ = open(os.path.join(os.path.dirname(__file__), "README.md"),
-               encoding="utf-8", errors="xmlcharrefreplace").read()
 __author__ = "hell03end"
-__version__ = (0, 0, 1)
+__version__ = (0, 1, 0)
 __all__ = ("RUZ")
