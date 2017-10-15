530e24dc69f8524000c108bf7ff746d2ba2a63aa
Alex <eshobee@gmail.com>
2017-10-15 15:47:14 +0300

    update annotations and fix imports

--- ruz/__init__.py
+++ ruz/__init__.py
@@ -5,16 +5,15 @@
         from ruz import RUZ
 
         api = RUZ()
-        assert api.v == 2
-        assert api.get("buildings")
+        assert api.v == 1
+        assert api.schedule("mymail@edu.hse.ru")
 '''
 
 import os
 
-from .utils import RUZ_API_ENDPOINTS, RUZ_API_URL
 from .RUZ import RUZ
-
+from .utils import REQUEST_SCHEMA, RESPONSE_SCHEMA
 
 __author__ = "hell03end"
 __version__ = (0, 1, 0)
-__all__ = ("RUZ")
+__all__ = ("RUZ", "REQUEST_SCHEMA", "RESPONSE_SCHEMA")
--- ruz/utils/__init__.py
+++ ruz/utils/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 from .logging import Logger, LEVELS
-from .schema import REQUEST_SCHEMA
+from .schema import REQUEST_SCHEMA, RESPONSE_SCHEMA
 
 
 RUZ_API_URL = os.environ.get("API_RUZ_URL")
