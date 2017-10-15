a23cb41ac7fcd6c3374ce8e33f87cfda27744b54
Alex <eshobee@gmail.com>
2017-10-15 18:37:40 +0300

    add schema loading from dump to reduce module size (raw schema is stored in raw_schema.py).

--- ruz/utils/schema.py
+++ ruz/utils/schema.py
@@ -1,222 +1,39 @@
-REQUEST_SCHEMA = {
-    'schedule': {
-        'from_date': str,
-        'fromDate': str,
-        'to_date': str,
-        'toDate': str,
-        'receiverType': int,
-        'groupOid': int,
-        'lecturerOid': int,
-        'auditoriumOid': int,
-        'studentOid': int,
-        'email': str
-    },
-    'groups': {
-        'facultyOid': int,
-        'findText': str
-    },
-    'staffOfGroup': {
-        'group_id': int,
-        'groupOid': int,
-        'findText': str
-    },
-    'streams': {'findText': str},
-    'staffOfStreams': {
-        'streamOid': int,
-        'stream_id': int
-    },
-    'lecturers': {
-        'chairOid': int,
-        'findText': str
-    },
-    'auditoriums': {
-        'buildingOid': int,
-        'findText': str
-    },
-    'typeOfAuditoriums': {},
-    'kindOfWorks': {},
-    'buildings': {'findText': str},
-    'faculties': {'findText': str},
-    'chairs': {
-        'facultyOid': int,
-        'findText': str
-    },
-    'subGroups': {'findText': str}
-}
+import pickle
+import os
 
 
-RESPONSE_SCHEMA = {
-    'schedule': [
-        {
-            'auditorium': str,
-            'auditoriumOid': int,
-            'beginLesson': str,
-            'building': str,
-            'date': str,
-            'dateOfNest': str,
-            'dayOfWeek': int,
-            'dayOfWeekString': str,
-            'detailInfo': str,
-            'discipline': str,
-            'disciplineinplan': str,
-            'disciplinetypeload': int,
-            'endLesson': str,
-            'group': None,
-            'groupOid': int,
-            'isBan': bool,
-            'kindOfWork': str,
-            'lecturer': str,
-            'lecturerOid': int,
-            'stream': str,
-            'streamOid': int,
-            'subGroup': None,
-            'subGroupOid': int
-        }
-    ],
-    'schedule2': {
-        'Count': int,
-        'Lessons': [
-            {
-                'auditorium': str,
-                'auditoriumOid': int,
-                'beginLesson': str,
-                'building': str,
-                'date': str,
-                'dateOfNest': str,
-                'dayOfWeek': int,
-                'dayOfWeekString': str,
-                'detailInfo': str,
-                'discipline': str,
-                'disciplineinplan': str,
-                'disciplinetypeload': int,
-                'endLesson': str,
-                'group': None,
-                'groupOid': int,
-                'isBan': bool,
-                'kindOfWork': str,
-                'lecturer': str,
-                'lecturerOid': int,
-                'stream': str,
-                'streamOid': int,
-                'subGroup': None,
-                'subGroupOid': int
-            }
-        ],
-        'StatusCode': {
-            'Code': int,
-            'Description': str
-        }
-    },  # api v2
-    'groups': [
-        {
-            'chairOid': int,
-            'course': int,
-            'faculty': str,
-            'facultyOid': int,
-            'formOfEducation': str,
-            'groupOid': int,
-            'kindEducation': int,
-            'number': str,
-            'speciality': str
-        }
-    ],
-    'staffOfGroup': [
-        {
-            'fio': str,
-            'shortFIO': str,
-            'studentOid': int
-        }
-    ],
-    'streams': [
-        {
-            'abbr': str,
-            'course': str,
-            'faculty': str,
-            'facultyOid': int,
-            'formOfEducation': str,
-            'name': str,
-            'streamOid': int,
-            'yearOfEducation': int
-        }
-    ],
-    'staffOfStreams': [
-        {
-            "GroupNumber": str,
-            "GroupOid": int,
-            "SubgroupName": str,
-            "SubgroupOid": int
-        }
-    ],
-    'lecturers': [
-        {
-            'chair': str,
-            'chairOid': int,
-            'fio': str,
-            'lecturerOid': int,
-            'shortFIO': str
-        }
-    ],
-    'auditoriums': [
-        {
-            'auditoriumOid': int,
-            'building': str,
-            'buildingOid': int,
-            'number': str,
-            'typeOfAuditorium': str
-        }
-    ],
-    'typeOfAuditoriums': [
-        {
-            'abbr': str,
-            'code': str,
-            'name': str,
-            'typeOfAuditoriumOid': int
-        }
-    ],
-    'kindOfWorks': [
-        {
-            'abbr': str,
-            'code': None,
-            'complexity': int,
-            'kindOfWorkOid': int,
-            'name': str,
-            'unit': str
-        }
-    ],
-    'buildings': [
-        {
-            'abbr': str,
-            'address': str,
-            'buildingOid': int,
-            'name': str
-        }
-    ],
-    'faculties': [
-        {
-            'abbr': str,
-            'code': None,
-            'facultyOid': int,
-            'institute': str,
-            'name': str
-        }
-    ],
-    'chairs': [
-        {
-            'abbr': str,
-            'chairOid': int,
-            'code': str,
-            'faculty': str,
-            'facultyOid': int,
-            'name': str
-        }
-    ],
-    'subGroups': [
-        {
-            'abbr': str,
-            'group': str,
-            'groupOid': int,
-            'name': str,
-            'subGroupOid': int
-        }
-    ]
-}
+def load_dump(path: str) -> object:
+    ''' load object from pickle dump '''
+    if os.path.exists(path):
+        with open(path, 'rb') as reader:
+            return pickle.load(reader)
+
+
+def save_dump(path: str, obj: object, rewrite: bool=False) -> bool:
+    ''' save object to pickle dump '''
+    if not os.path.exists(path) or rewrite:
+        with open(path, 'wb') as writer:
+            pickle.dump(obj, writer)
+            return True
+    return False
+
+
+DUMPS_PATH = os.path.join(os.path.dirname(__file__), "dumps")
+REQ_DUMP_PATH = os.path.join(DUMPS_PATH, "REQUEST_SCHEMA.dump")
+RESP_DUMP_PATH = os.path.join(DUMPS_PATH, "RESPONSE_SCHEMA.dump")
+
+
+REQUEST_SCHEMA = load_dump(REQ_DUMP_PATH)
+assert REQUEST_SCHEMA
+
+RESPONSE_SCHEMA = load_dump(RESP_DUMP_PATH)
+assert RESPONSE_SCHEMA
+
+
+__all__ = ("REQUEST_SCHEMA", "RESPONSE_SCHEMA")
+
+
+if __name__ == "__main__":
+    from .raw_schema import REQUEST_SCHEMA, RESPONSE_SCHEMA
+    save_dump(REQ_DUMP_PATH, REQUEST_SCHEMA, rewrite=False)
+    save_dump(RESP_DUMP_PATH, RESPONSE_SCHEMA, rewrite=False)
