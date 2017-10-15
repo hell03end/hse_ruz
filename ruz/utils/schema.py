a255434ed74845f0dbcbefd7462769e92f6f7ab8
Alex <eshobee@gmail.com>
2017-10-15 15:18:27 +0300

    (bug) fix request schema, complete response schema

--- ruz/utils/schema.py
+++ ruz/utils/schema.py
@@ -17,10 +17,14 @@ REQUEST_SCHEMA = {
     },
     'staffOfGroup': {
         'group_id': int,
+        'groupOid': int,
         'findText': str
     },
     'streams': {'findText': str},
-    'staffOfStreams': {'stream_id': int},
+    'staffOfStreams': {
+        'streamOid': int,
+        'stream_id': int
+    },
     'lecturers': {
         'chairOid': int,
         'findText': str
@@ -103,4 +107,109 @@ RESPONSE_SCHEMA = {
             'Description': str
         }
     },  # api v2
+    'groups': [
+        {
+            'chairOid': int,
+            'course': int,
+            'faculty': str,
+            'facultyOid': int,
+            'formOfEducation': str,
+            'groupOid': int,
+            'kindEducation': int,
+            'number': str,
+            'speciality': str
+        }
+    ],
+    'staffOfGroup': [
+        {
+            'fio': str,
+            'shortFIO': str,
+            'studentOid': int
+        }
+    ],
+    'streams': [
+        {
+            'abbr': str,
+            'course': str,
+            'faculty': str,
+            'facultyOid': int,
+            'formOfEducation': str,
+            'name': str,
+            'streamOid': int,
+            'yearOfEducation': int
+        }
+    ],
+    'staffOfStreams': {},
+    'lecturers': [
+        {
+            'chair': str,
+            'chairOid': int,
+            'fio': str,
+            'lecturerOid': int,
+            'shortFIO': str
+        }
+    ],
+    'auditoriums': [
+        {
+            'auditoriumOid': int,
+            'building': str,
+            'buildingOid': int,
+            'number': str,
+            'typeOfAuditorium': str
+        }
+    ],
+    'typeOfAuditoriums': [
+        {
+            'abbr': str,
+            'code': str,
+            'name': str,
+            'typeOfAuditoriumOid': int
+        }
+    ],
+    'kindOfWorks': [
+        {
+            'abbr': str,
+            'code': None,
+            'complexity': int,
+            'kindOfWorkOid': int,
+            'name': str,
+            'unit': str
+        }
+    ],
+    'buildings': [
+        {
+            'abbr': str,
+            'address': str,
+            'buildingOid': int,
+            'name': str
+        }
+    ],
+    'faculties': [
+        {
+            'abbr': str,
+            'code': None,
+            'facultyOid': int,
+            'institute': str,
+            'name': str
+        }
+    ],
+    'chairs': [
+        {
+            'abbr': str,
+            'chairOid': int,
+            'code': str,
+            'faculty': str,
+            'facultyOid': int,
+            'name': str
+        }
+    ],
+    'subGroups': [
+        {
+            'abbr': str,
+            'group': str,
+            'groupOid': int,
+            'name': str,
+            'subGroupOid': int
+        }
+    ]
 }
