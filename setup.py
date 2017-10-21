91a29ea4cf7108c2b7678b976d5d81619fad5918
Alex <eshobee@gmail.com>
2017-10-21 18:02:44 +0300

    update to v0.2.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except ImportError:
 setup(
     name="hse_ruz",
     packages=packages,
-    version="0.2.1",
+    version="0.2.2",
     description="Python wrapper for HSE RUZ API",
     long_description=open(os.path.join(os.path.dirname(__file__),
                                        "README.rst")).read(),
