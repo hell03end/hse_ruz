fc82532418f31a7a859469b1517452df7100e9aa
Alex <eshobee@gmail.com>
2017-10-22 00:03:44 +0300

    remove schema dump

--- ruz/utils/schema.py
+++ ruz/utils/schema.py
@@ -1,57 +1,255 @@
-import pickle
-import os
+RUZ_API_ENDPOINTS = {
+    'schedule': "personLessons",
+    'groups': "groups",
+    'staffOfGroup': "staffOfGroup",
+    'staff_of_group': "staffOfGroup",
+    'streams': "streams",
+    'staffOfStreams': "staffOfStreams",
+    'staff_of_streams': "staffOfStreams",
+    'lecturers': "lecturers",
+    'auditoriums': "auditoriums",
+    'typeOfAuditoriums': "typeOfAuditoriums",
+    'type_of_auditoriums': "typeOfAuditoriums",
+    'kindOfWorks': "kindOfWorks",
+    'kind_of_works': "kindOfWorks",
+    'buildings': "buildings",
+    'faculties': "faculties",
+    'chairs': "chairs",
+    'subGroups': "subGroups",
+    'subgroups': "subGroups",
+    'sub_groups': "subGroups"
+}
 
-
-def load_dump(path: str) -> object:
-    ''' load object from pickle dump '''
-    if os.path.exists(path):
-        with open(path, 'rb') as reader:
-            return pickle.load(reader)
-
-
-def save_dump(path: str, obj: object, rewrite: bool=False) -> bool:
-    ''' save object to pickle dump '''
-    if not os.path.exists(path) or rewrite:
-        with open(path, 'wb') as writer:
-            pickle.dump(obj, writer)
-            return True
-    return False
-
-
-def recreate_dump(force: bool=False) -> None:
-    ''' create new dump files '''
-    if not os.path.exists(DUMPS_PATH) or force:
-        from .raw_schema import (REQUEST_SCHEMA, RESPONSE_SCHEMA,
-                                 RUZ_API_ENDPOINTS)
-
-        if not os.path.exists(DUMPS_PATH):
-            os.makedirs(DUMPS_PATH)
-        save_dump(REQ_DUMP_PATH, REQUEST_SCHEMA, rewrite=True)
-        save_dump(RESP_DUMP_PATH, RESPONSE_SCHEMA, rewrite=True)
-        save_dump(ENDPOINTS_DUMP_PATH, RUZ_API_ENDPOINTS, rewrite=True)
-
-
-DUMPS_PATH = os.path.join(os.path.dirname(__file__), "dumps")
-REQ_DUMP_PATH = os.path.join(DUMPS_PATH, "REQUEST_SCHEMA.dump")
-RESP_DUMP_PATH = os.path.join(DUMPS_PATH, "RESPONSE_SCHEMA.dump")
-ENDPOINTS_DUMP_PATH = os.path.join(DUMPS_PATH, "RUZ_API_ENDPOINTS.dump")
-
-
-recreate_dump()
-
-
-REQUEST_SCHEMA = load_dump(REQ_DUMP_PATH)
-assert REQUEST_SCHEMA
-
-RESPONSE_SCHEMA = load_dump(RESP_DUMP_PATH)
-assert RESPONSE_SCHEMA
-
-RUZ_API_ENDPOINTS = load_dump(ENDPOINTS_DUMP_PATH)
-assert RUZ_API_ENDPOINTS
-
-
-__all__ = ("REQUEST_SCHEMA", "RESPONSE_SCHEMA", "RUZ_API_ENDPOINTS")
-
-
-if __name__ == "__main__":
-    recreate_dump(force=True)
+REQUEST_SCHEMA = {
+    'schedule': {
+        'from_date': str,
+        'fromDate': str,
+        'to_date': str,
+        'toDate': str,
+        'receiverType': int,
+        'groupOid': int,
+        'lecturerOid': int,
+        'auditoriumOid': int,
+        'studentOid': int,
+        'email': str
+    },
+    'groups': {
+        'facultyOid': int,
+        'findText': str
+    },
+    'staffOfGroup': {
+        'group_id': int,
+        'groupOid': int,
+        'findText': str
+    },
+    'staff_of_group': {
+        'group_id': int,
+        'groupOid': int,
+        'findText': str
+    },
+    'streams': {'findText': str},
+    'staffOfStreams': {
+        'streamOid': int,
+        'stream_id': int
+    },
+    'staff_of_streams': {
+        'streamOid': int,
+        'stream_id': int
+    },
+    'lecturers': {
+        'chairOid': int,
+        'findText': str
+    },
+    'auditoriums': {
+        'buildingOid': int,
+        'findText': str
+    },
+    'typeOfAuditoriums': {},
+    'type_of_auditoriums': {},
+    'kindOfWorks': {},
+    'kind_of_works': {},
+    'buildings': {'findText': str},
+    'faculties': {'findText': str},
+    'chairs': {
+        'facultyOid': int,
+        'findText': str
+    },
+    'subGroups': {'findText': str},
+    'subgroups': {'findText': str},
+    'sub_groups': {'findText': str}
+}
+RESPONSE_SCHEMA = {
+    'schedule': [
+        {
+            'auditorium': str,
+            'auditoriumOid': int,
+            'beginLesson': str,
+            'building': str,
+            'date': str,
+            'dateOfNest': str,
+            'dayOfWeek': int,
+            'dayOfWeekString': str,
+            'detailInfo': str,
+            'discipline': str,
+            'disciplineinplan': str,
+            'disciplinetypeload': int,
+            'endLesson': str,
+            'group': str(type(None)),
+            'groupOid': int,
+            'isBan': bool,
+            'kindOfWork': str,
+            'lecturer': str,
+            'lecturerOid': int,
+            'stream': str,
+            'streamOid': int,
+            'subGroup': (str, type(None)),
+            'subGroupOid': int
+        }
+    ],
+    'schedule2': {
+        'Count': int,
+        'Lessons': [
+            {
+                'auditorium': str,
+                'auditoriumOid': int,
+                'beginLesson': str,
+                'building': str,
+                'date': str,
+                'dateOfNest': str,
+                'dayOfWeek': int,
+                'dayOfWeekString': str,
+                'detailInfo': str,
+                'discipline': str,
+                'disciplineinplan': str,
+                'disciplinetypeload': int,
+                'endLesson': str,
+                'group': str(type(None)),
+                'groupOid': int,
+                'isBan': bool,
+                'kindOfWork': str,
+                'lecturer': str,
+                'lecturerOid': int,
+                'stream': str,
+                'streamOid': int,
+                'subGroup': str(type(None)),
+                'subGroupOid': int
+            }
+        ],
+        'StatusCode': {
+            'Code': int,
+            'Description': str
+        }
+    },  # api v2
+    'groups': [
+        {
+            'chairOid': int,
+            'course': int,
+            'faculty': str,
+            'facultyOid': int,
+            'formOfEducation': str,
+            'groupOid': int,
+            'kindEducation': int,
+            'number': str,
+            'speciality': str
+        }
+    ],
+    'staffOfGroup': [
+        {
+            'fio': str,
+            'shortFIO': str,
+            'studentOid': int
+        }
+    ],
+    'streams': [
+        {
+            'abbr': str,
+            'course': str,
+            'faculty': str,
+            'facultyOid': int,
+            'formOfEducation': str,
+            'name': str,
+            'streamOid': int,
+            'yearOfEducation': int
+        }
+    ],
+    'staffOfStreams': [
+        {
+            "GroupNumber": str,
+            "GroupOid": int,
+            "SubgroupName": str,
+            "SubgroupOid": int
+        }
+    ],
+    'lecturers': [
+        {
+            'chair': str,
+            'chairOid': int,
+            'fio': str,
+            'lecturerOid': int,
+            'shortFIO': str
+        }
+    ],
+    'auditoriums': [
+        {
+            'auditoriumOid': int,
+            'building': str,
+            'buildingOid': int,
+            'number': str,
+            'typeOfAuditorium': str
+        }
+    ],
+    'typeOfAuditoriums': [
+        {
+            'abbr': str,
+            'code': str,
+            'name': str,
+            'typeOfAuditoriumOid': int
+        }
+    ],
+    'kindOfWorks': [
+        {
+            'abbr': str,
+            'code': type(None),  # TODO: add correct type
+            'complexity': int,
+            'kindOfWorkOid': int,
+            'name': str,
+            'unit': str
+        }
+    ],
+    'buildings': [
+        {
+            'abbr': str,
+            'address': str,
+            'buildingOid': int,
+            'name': str
+        }
+    ],
+    'faculties': [
+        {
+            'abbr': str,
+            'code': type(None),
+            'facultyOid': int,
+            'institute': str,
+            'name': str
+        }
+    ],
+    'chairs': [
+        {
+            'abbr': str,
+            'chairOid': int,
+            'code': str,
+            'faculty': str,
+            'facultyOid': int,
+            'name': str
+        }
+    ],
+    'subGroups': [
+        {
+            'abbr': str,
+            'group': str,
+            'groupOid': int,
+            'name': str,
+            'subGroupOid': int
+        }
+    ]
+}
