b82462c5127bdbe952921db29af68c5f153ad939
Alex <eshobee@gmail.com>
2017-10-25 21:12:48 +0300

    improve Logger, add log decorator to debug

--- ruz/utils/logging.py
+++ ruz/utils/logging.py
@@ -1,22 +1,37 @@
-LEVELS = {
-    "NOTSET": 0,
-    "DEBUG": 10,
-    "INFO": 20,
-    "WARN": 30,
-    "WARNING": 30,
-    "ERROR": 40,
-    "FATAL": 50,
-    "CRITICAL": 50
-}
+import logging
+from collections import Callable
+from functools import wraps
 
 
-def Logger(name: str, level: int=LEVELS["DEBUG"], **kwargs) -> ...:
-    import logging
+def Logger(name: str, level: int=logging.INFO, **kwargs) -> logging.RootLogger:
     logging.basicConfig(
-        format=kwargs.get(
+        format=kwargs.pop(
             "format",
             "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
         ),
         level=level
     )
     return logging.getLogger(name)
+
+
+def log(func: Callable, level: int=logging.DEBUG, **kwargs) -> Callable:
+    """ Log function entering, arguments and exiting (to debug) """
+    logger = Logger(
+        "{}::{}".format(func.__module__, func.__name__),
+        level,
+        **kwargs
+    )
+    with_args = kwargs.pop("with_args", True)
+
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> object:
+        logger.debug("Entering: %s", func.__name__)
+        if with_args:
+            for arg in args:
+                logger.debug(arg)
+            for key, value in kwargs.items():
+                logger.debug("%s = %s", key, value)
+        result = func(*args, **kwargs)
+        logger.debug("Exiting: %s", func.__name__)
+        return result
+    return wrapper
