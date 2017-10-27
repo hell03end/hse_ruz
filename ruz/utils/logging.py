365e3acae3973dc8fee795312ecd7a5990144af4
Alex <eshobee@gmail.com>
2017-10-27 12:56:00 +0300

    remove redundant params, update annotations and comments, add doctest

--- ruz/utils/logging.py
+++ ruz/utils/logging.py
@@ -4,6 +4,30 @@ from functools import wraps
 
 
 def Logger(name: str, level: int=logging.INFO, **kwargs) -> logging.RootLogger:
+    """
+        Creates configured logger
+
+        :param name, required - name for logger.
+        :param level - logging level.
+        :param format, str - logging format.
+
+        Usage
+        -----
+        logger = Logger(__name__)
+
+        >>> Logger("some name").info("Hello, world!")
+        ... - some name - INFO - Hello, world!
+        >>> Logger()
+        Traceback (most recent call last):
+            ...
+        TypeError: Logger() missing 1 required positional argument: 'name'
+        >>> Logger(123)
+        Traceback (most recent call last):
+            ...
+        ValueError: Expect str, got: <class 'int'>
+    """
+    if not isinstance(name, str):
+        raise ValueError("Expect str, got: {}".format(type(name)))
     logging.basicConfig(
         format=kwargs.pop(
             "format",
@@ -14,24 +38,36 @@ def Logger(name: str, level: int=logging.INFO, **kwargs) -> logging.RootLogger:
     return logging.getLogger(name)
 
 
-def log(func: Callable, level: int=logging.DEBUG, **kwargs) -> Callable:
-    """ Log function entering, arguments and exiting (to debug) """
+def log(func: Callable) -> Callable:
+    """
+        Log function entering, arguments and exiting (to debug)
+
+        :param func, required - wrapped function/method.
+
+        Usage
+        -----
+        @log
+        def some_func():
+            pass
+    """
     logger = Logger(
-        "{}::{}".format(func.__module__, func.__name__),
-        level,
-        **kwargs
+        name="{}::{}".format(func.__module__, func.__name__),
+        level=logging.DEBUG
     )
-    with_args = kwargs.pop("with_args", True)
 
     @wraps(func)
     def wrapper(*args, **kwargs) -> object:
         logger.debug("Entering: %s", func.__name__)
-        if with_args:
-            for arg in args:
-                logger.debug(arg)
-            for key, value in kwargs.items():
-                logger.debug("%s = %s", key, value)
+        for arg in args:
+            logger.debug("arg::%s", arg)
+        for key, value in kwargs.items():
+            logger.debug("kwarg::%s=%s", key, value)
         result = func(*args, **kwargs)
         logger.debug("Exiting: %s", func.__name__)
         return result
     return wrapper
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
