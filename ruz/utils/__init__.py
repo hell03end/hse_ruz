2c7147dbe0f17f63b08176a28d10d4ee7315115f
Alex <eshobee@gmail.com>
2017-10-27 13:01:27 +0300

    fix imports from utils

--- ruz/utils/__init__.py
+++ ruz/utils/__init__.py
@@ -1,9 +1,10 @@
 import os
 
-from ruz.utils.logging import Logger, LEVELS
+from ruz.utils.decorators import none_safe
+from ruz.utils.logging import Logger, log
 from ruz.utils.schema import REQUEST_SCHEMA, RESPONSE_SCHEMA, RUZ_API_ENDPOINTS
 
 
-RUZ_API_URL = "http://92.242.58.221/ruzservice.svc/"
+RUZ_API_URL = r"http://92.242.58.221/ruzservice.svc/"
 EMAIL_PATTERN = r"\b[a-zA-Z0-9\._-]{2,}@([a-zA-Z]{2,}\.)?[a-zA-Z]{2,}\.ru\b"
 EMAIL_DOMAINS = ("hse.ru", "edu.hse.ru")
