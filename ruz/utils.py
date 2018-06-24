300d5c5c1386e8e429f19378706effa1ddc43a47
Dmitriy <hell03end@users.noreply.github.com>
2018-06-25 01:41:03 +0300

    Develop/main (#14)
    migrate to **v2.1.0**:
    * update response schema
    * use only debug-level logging
    * rearrange and simplify code

--- ruz/utils.py
+++ ruz/utils.py
@@ -1,36 +1,191 @@
+import json
+import logging
 import os
+import re
 from collections import Callable
+from datetime import datetime, timedelta
 from functools import wraps
+from urllib import error, parse, request
 
+from ruz.logging import log
+from ruz.schema import API_ENDPOINTS, API_URL, REQUEST_SCHEMA
 
-# ===== Constants =====
-
-RUZ_API_URL = os.environ.get("RUZ_API_URL",
-                             r"http://92.242.58.221/ruzservice.svc/")
-
-# detect RUZ API version from possible RUZ API URLs
-RUZ_API_V = 1
-if RUZ_API_URL == r"http://92.242.58.221/ruzservice.svc/v2/":
-    RUZ_API_V = 2
-elif RUZ_API_URL == r"https://www.hse.ru/api/":
-    RUZ_API_V = 3
-
-# default values
 CHECK_EMAIL_ONLINE = bool(os.environ.get("CHECK_EMAIL_ONLINE", False))
 USE_NONE_SAFE_VALUES = bool(os.environ.get("USE_NONE_SAFE_VALUES", True))
+HSE_EMAIL_REGEX = re.compile(r"^[a-z0-9\._-]{3,}@(edu\.)?hse\.ru$")
 
 
-# ===== Decorators =====
-
-def none_safe(args: bool=True, kwargs: bool=True) -> Callable:
+def none_safe(func: Callable) -> Callable:
     """ Pass only not None args/kwargs to function """
-    def decor(func: Callable) -> Callable:
-        @wraps(func)
-        def wrapper(*_args, **_kwargs) -> object:
-            if args:
-                _args = [arg for arg in _args if arg is not None]
-            if kwargs:
-                _kwargs = {k: v for k, v in _kwargs.items() if v is not None}
-            return func(*_args, **_kwargs)
-        return wrapper
-    return decor
+    @wraps(func)
+    def wrapper(*args, **kwargs) -> object:
+        args = [arg for arg in args if arg is not None]
+        kwargs = {k: v for k, v in kwargs.items() if v is not None}
+        return func(*args, **kwargs)
+    return wrapper
+
+
+def is_student(email: str) -> bool or None:
+    """
+        Check email belongs to student
+
+        :param email, required - valid HSE email addres or domain.
+
+        Stutent's domain:   @edu.hse.ru
+        HSE stuff' domain:  @hse.ru
+    """
+    email_domain = email.lower().split("@")[-1]
+
+    if email_domain == "edu.hse.ru":
+        return True
+    elif email_domain == "hse.ru":
+        return False
+
+    logging.debug("Wrong HSE email domain: '%s'", email_domain)
+
+
+def is_hse_email(email: str) -> bool:
+    """
+        Check email is valid HSE corp. email
+
+        :param email, required - email address to check.
+    """
+    if HSE_EMAIL_REGEX.fullmatch(email.lower()):
+        return True
+    logging.debug("Incorrect HSE email '%s'.", email)
+    return False
+
+
+def get_formated_date(day_bias: int or float=0) -> str:
+    """
+        Return date in RUZ API compatible format
+
+        :param day_bias - number of day from now.
+    """
+    return (datetime.now() + timedelta(
+        days=float(day_bias)
+    )).strftime("%Y.%m.%d")
+
+
+@log
+def is_valid_hse_email(email: str) -> bool:
+    """
+        Check email is valid via API endpoint call (schedule)
+
+        :param email - email address to check (for schedules only).
+    """
+    @none_safe
+    def request_schedule_api(**params) -> list or dict:
+        return request.urlopen(make_url(
+            "schedule",
+            email=email,
+            fromDate=get_formated_date(),
+            toDate=get_formated_date(1),
+            **params
+        ))
+
+    email = email.strip().lower()
+    if not is_hse_email(email):
+        return False
+
+    try:
+        response = request_schedule_api(
+            receiverType=1 if not is_student(email) else None
+        )
+        del response
+    except (error.HTTPError, error.URLError) as err:
+        logging.debug("Email '%s' wasn't verified.\n%s", email, err)
+        return False
+    return True
+
+
+@log
+def is_valid_schema(endpoint: str,
+                    check_email_online: bool=CHECK_EMAIL_ONLINE,
+                    **params) -> bool:
+    """
+        Check params fit schema for certain endpoint
+
+        :param endpoint - endpoint for request.
+        :param check_email_online - use is_valid_hse_email.
+        :param params - schema params.
+    """
+
+    if (endpoint == "schedule" and "lecturerOid" not in params and
+            "studentOid" not in params and "email" not in params and
+            "auditoriumOid" not in params):
+        logging.debug("One of the followed required: lecturer_id, "
+                      "auditorium_id, student_id, email for "
+                      "schedule endpoint.")
+        return False
+
+    if params.get('email') is not None:
+        email = params['email']
+        if not is_hse_email(email):
+            del email
+            return False
+        elif check_email_online and not is_valid_hse_email(email):
+            logging.debug("'%s' is not verified by API call.", email)
+        del email
+
+    endpoint = API_ENDPOINTS.get(endpoint)
+    if endpoint is None:
+        logging.debug("Can't find endpoint: '%s'.", endpoint)
+        del endpoint
+        return False
+
+    schema = REQUEST_SCHEMA[endpoint]
+    for key, value in params.items():
+        if key not in schema:
+            logging.debug("Can't find '%s' schema param: '%s'", endpoint, key)
+            del schema, endpoint
+            return False
+        if not isinstance(value, schema[key]):
+            logging.debug("Expected {} for '{}'::'{}' got: {}",
+                          schema[key], endpoint, key, type(value))
+            del schema, endpoint
+            return False
+    del schema, endpoint
+    return True
+
+
+@log
+def make_url(endpoint: str, **params) -> str:
+    """
+        Creates URL for API requests
+
+        :param endpoint - endpoint for request.
+        :param params - request params.
+    """
+    url = "".join((API_URL, API_ENDPOINTS[endpoint]))
+    if params:
+        return "?".join((url, parse.urlencode(params)))
+    return url
+
+
+@none_safe
+@log
+def get(endpoint: str,
+        encoding: str="utf-8",
+        return_none_safe: bool=USE_NONE_SAFE_VALUES,
+        **params) -> (list, dict, None):
+    """
+        Return requested data in JSON
+
+        Check request has correct schema.
+
+        :param endpoint - endpoint for request.
+        :param encoding - encoding for received data.
+        :param return_none_safe - return empty list on fallback.
+        :param params - requested params
+    """
+    if not is_valid_schema(endpoint, **params):
+        return [] if return_none_safe else None
+
+    url = make_url(endpoint, **params)
+    try:
+        response = request.urlopen(url)
+        return json.loads(response.read().decode(encoding))
+    except (error.HTTPError, error.URLError) as err:
+        logging.debug("Can't get '%s'.\n%s", url, err)
+    return [] if return_none_safe else None
--- tests/test_RUZ.py
+++ tests/test_RUZ.py
@@ -3,110 +3,105 @@
 import re
 
 import pytest
+
+import ruz
+from ruz.schema import API_ENDPOINTS
+from ruz.utils import API_URL
 from tests import (INTRUSTED_EMAILS, TRUSTED_EMAILS, TRUSTED_LECTURER_ID,
                    logging)
 
-import ruz
-from ruz.schema import RUZ_API_ENDPOINTS
-from ruz.utils import RUZ_API_URL
-
-
-# ===== Common methods =====
 
 def test_is_student():
-    assert not ruz.is_student(TRUSTED_EMAILS['lecturer'])
-    assert ruz.is_student(TRUSTED_EMAILS['student'])
-    assert ruz.is_student(INTRUSTED_EMAILS['other']) is None
+    assert not ruz.utils.is_student(TRUSTED_EMAILS['lecturer'])
+    assert ruz.utils.is_student(TRUSTED_EMAILS['student'])
+    assert ruz.utils.is_student(INTRUSTED_EMAILS['other']) is None
 
 
 def test_is_hse_email():
     for email in ("somemail@hse.com", "somem@il@edu.hse.ru",
                   "somemail@google.ru", INTRUSTED_EMAILS['other']):
-        assert not ruz.is_hse_email(email)
+        assert not ruz.utils.is_hse_email(email)
     for email in (INTRUSTED_EMAILS['hse'], TRUSTED_EMAILS['student'],
                   TRUSTED_EMAILS['lecturer']):
-        assert ruz.is_hse_email(email)
+        assert ruz.utils.is_hse_email(email)
 
 
 # minimal functionality test
 def test_is_valid_hse_email():
     for email in TRUSTED_EMAILS.values():
-        assert ruz.is_valid_hse_email(email)
+        assert ruz.utils.is_valid_hse_email(email)
     for email in INTRUSTED_EMAILS.values():
-        assert not ruz.is_valid_hse_email(email)
+        assert not ruz.utils.is_valid_hse_email(email)
 
 
 def test_get_formated_date():
     with pytest.raises(ValueError):
-        ruz.get_formated_date("x")
+        ruz.utils.get_formated_date("x")
 
     for bias in (1, 1.1, 1., "1", "1.1", -1, 0, "-1", -1, "-1.", "01."):
-        ruz_date = ruz.get_formated_date(bias)
+        ruz_date = ruz.utils.get_formated_date(bias)
         assert ruz_date
         assert isinstance(ruz_date, str)
         assert len(ruz_date) == 10
         assert re.match(r"[\d]{4}(\.)[\d]{2}\1[\d]{2}", ruz_date)
 
-    assert ruz.get_formated_date(0) > ruz.get_formated_date(-1)
-    assert ruz.get_formated_date(0) < ruz.get_formated_date(1)
-    assert ruz.get_formated_date(0) == ruz.get_formated_date(0.000001)
+    assert ruz.utils.get_formated_date(0) > ruz.utils.get_formated_date(-1)
+    assert ruz.utils.get_formated_date(0) < ruz.utils.get_formated_date(1)
+    assert ruz.utils.get_formated_date(0) == \
+        ruz.utils.get_formated_date(0.000001)
 
-
-# ===== Special methods =====
 
 def test_make_url():
     with pytest.raises(KeyError):
-        ruz.main.make_url("")
-    for key, endpoint in RUZ_API_ENDPOINTS.items():
-        assert ruz.main.make_url(key) == RUZ_API_URL + endpoint
-        assert ruz.main.make_url(key, v=1) == "?".join((
-            RUZ_API_URL + endpoint, "v=1"
+        ruz.utils.make_url("")
+    for key, endpoint in API_ENDPOINTS.items():
+        assert ruz.utils.make_url(key) == API_URL + endpoint
+        assert ruz.utils.make_url(key, v=1) == "?".join((
+            API_URL + endpoint, "v=1"
         ))
 
 
 # tests not all cases (type of param, etc.)
 def test_is_valid_schema():
-    assert not ruz.main.is_valid_schema("")
-    for endpoint in RUZ_API_ENDPOINTS.keys():
+    assert not ruz.utils.is_valid_schema("")
+    for endpoint in API_ENDPOINTS.keys():
         if endpoint == 'schedule':
-            assert not ruz.main.is_valid_schema(
+            assert not ruz.utils.is_valid_schema(
                 endpoint, email=INTRUSTED_EMAILS['other']
             )
-            assert not ruz.main.is_valid_schema(endpoint)
+            assert not ruz.utils.is_valid_schema(endpoint)
             for email in TRUSTED_EMAILS.values():
-                assert ruz.main.is_valid_schema(endpoint, email=email,
-                                                check_email_online=False)
+                assert ruz.utils.is_valid_schema(endpoint, email=email,
+                                                 check_email_online=False)
         else:
-            assert not ruz.main.is_valid_schema(endpoint, tmp=None)
-            assert ruz.main.is_valid_schema(endpoint)
+            assert not ruz.utils.is_valid_schema(endpoint, tmp=None)
+            assert ruz.utils.is_valid_schema(endpoint)
 
 
 # tests only negative cases, positive cases are tested in test_schema
 def test_get():
-    for endpoint in RUZ_API_ENDPOINTS.keys():
+    for endpoint in API_ENDPOINTS.keys():
         if endpoint == "schedule":
-            none_safe = ruz.main.get(
+            none_safe = ruz.utils.get(
                 endpoint,
                 email=INTRUSTED_EMAILS['other'],
                 return_none_safe=True,
                 check_email_online=False
             )
-            usual = ruz.main.get(
+            usual = ruz.utils.get(
                 endpoint,
                 email=INTRUSTED_EMAILS['other'],
                 return_none_safe=False,
                 check_email_online=False
             )
         else:
-            none_safe = ruz.main.get(endpoint, tmp=123, return_none_safe=True)
-            usual = ruz.main.get(endpoint, tmp=123, return_none_safe=False)
+            none_safe = ruz.utils.get(endpoint, tmp=123, return_none_safe=True)
+            usual = ruz.utils.get(endpoint, tmp=123, return_none_safe=False)
         assert not none_safe
         assert isinstance(none_safe, list)
         assert not usual
         assert usual is None
 
-
-# ===== API methods =====
 
 def test_schedules():
     with pytest.raises(ValueError) as excinfo:
